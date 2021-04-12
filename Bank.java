a349c3a6c942e5b9130a91de90a3bf970858d3a0
jamesongr <41484886+jamesongr@users.noreply.github.com>
2021-04-11 21:57:32 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,36 +1,69 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
 
-public class bank extends JFrame {
 
-    bank() {
-        super("Banking Transfer");
-        JLabel title = new JLabel("Banking Transfer", SwingConstants.CENTER);
-        title.setFont(new Font("Serif", Font.BOLD, 50));
-        add(title, BorderLayout.NORTH);
-        Container c = getContentPane();
+public class quz implements ActionListener
+{
+    JFrame f1=new JFrame("Online banking Log In");
+    JLabel l1,l2;
+    JTextField t1,t2;
+    JButton b1;
+
+    quz()
+    {
+        l1=new JLabel("User Name");
+        l2=new JLabel("Password");
+        t1=new JTextField(20);
+        t2=new JTextField(20);
+        b1=new JButton("OK");
+
+        f1.setSize(400,400);
+        f1.setVisible(true);
+        f1.setLayout(null);
+
+        f1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        f1.add(l1);
+        f1.add(t1);
+        f1.add(l2);
+        f1.add(t2);
+
+        f1.getContentPane().add(b1);
+        Container c = f1.getContentPane();
         c.setBackground(Color.LIGHT_GRAY);
 
-        Container content = getContentPane();
-        LayoutManager layout = new GridLayout(20, 500);
-        content.setLayout(layout);
+        l1.setBounds(0,30,100,30);
+        t1.setBounds(110,30,100,30);
+        l2.setBounds(0,70,100,30);
+        t2.setBounds(110,70,100,30);
+        b1.setBounds(0,120,100,30);
 
+        b1.addActionListener(this);
     }
 
+    public void actionPerformed(ActionEvent e)
+    {
+        f1.dispose();
+        Project1 p2=new Project1();
+    }
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                bank gui = new bank();
-                gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                gui.pack();
-                gui.setVisible(true);
-                                                   }
-                                               }
-        );
+    public static void main(String ag[])
+    {
+        quz p1=new quz();
     }
 }
 
+//Second JFrame
+
+
+class Project1
+{
+    JFrame f1=new JFrame("Banking");
+
+    Project1()
+    {
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setVisible(true);
+        f1.setSize(400,400);
+    }
+}
