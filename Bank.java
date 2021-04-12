557489f0439d3b86620a19e6431cc4901e349112
jamesongr <41484886+jamesongr@users.noreply.github.com>
2021-04-11 21:42:22 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,1 +1,36 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class bank extends JFrame {
+
+    bank() {
+        super("Banking Transfer");
+        JLabel title = new JLabel("Banking Transfer", SwingConstants.CENTER);
+        title.setFont(new Font("Serif", Font.BOLD, 50));
+        add(title, BorderLayout.NORTH);
+        Container c = getContentPane();
+        c.setBackground(Color.LIGHT_GRAY);
+
+        Container content = getContentPane();
+        LayoutManager layout = new GridLayout(20, 500);
+        content.setLayout(layout);
+
+    }
+
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                bank gui = new bank();
+                gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                gui.pack();
+                gui.setVisible(true);
+                                                   }
+                                               }
+        );
+    }
+}
 
