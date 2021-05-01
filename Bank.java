09ee08e97012e0a9de62328433f64c98a9063773
jamesongr <41484886+jamesongr@users.noreply.github.com>
2021-04-30 20:52:08 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -3,18 +3,23 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class quz implements ActionListener
+
+public class Bank implements ActionListener
 {
     JFrame f1=new JFrame("Online banking Log In");
     JLabel l1,l2;
     JTextField t1,t2;
     JButton b1;
 
-    quz()
+    Bank()
     {
         l1=new JLabel("User Name");
         l2=new JLabel("Password");
         t1=new JTextField(20);
+
+
+
+
         t2=new JTextField(20);
         b1=new JButton("OK");
 
@@ -39,7 +44,25 @@ public class quz implements ActionListener
         b1.setBounds(0,120,100,30);
 
         b1.addActionListener(this);
+
+
+
+        t1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("User Name is: " + t1.getText());
+            }
+        });
+
+
+        t2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Password is: " + t2.getText());
+            }
+        });
     }
+
 
     public void actionPerformed(ActionEvent e)
     {
@@ -49,7 +72,7 @@ public class quz implements ActionListener
 
     public static void main(String args[])
     {
-        quz p1=new quz();
+        Bank p1=new Bank();
     }
 }
 
@@ -63,7 +86,7 @@ class Project1 implements ActionListener {
     JButton b0;
     JLabel l3;
     JLabel l4;
-    JButton b2;
+
 
     Project1() {
         JLabel l3 = new JLabel("Online Banking", SwingConstants.CENTER);
@@ -74,7 +97,7 @@ class Project1 implements ActionListener {
 
 
         b0 = new JButton("Checking");
-        b2 = new JButton("Add a new account");
+
 
         f1.setSize(400, 400);
         f1.setVisible(true);
@@ -85,7 +108,7 @@ class Project1 implements ActionListener {
 
         f1.add(l3);
         f1.add(l4);
-        f1.add(b2);
+
 
         f1.getContentPane().add(b0);
         Container c = f1.getContentPane();
@@ -93,7 +116,7 @@ class Project1 implements ActionListener {
 
 
         b0.setBounds(145, 100, 100, 30);
-        b2.setBounds(110, 300, 200, 30);
+
 
 
         l3.setBounds(100, 10, 200, 30);
@@ -113,7 +136,7 @@ class Project1 implements ActionListener {
 
     public static void main(String args[])
     {
-        quz p3=new quz();
+        Bank p3=new Bank();
     }
 
 
@@ -169,6 +192,25 @@ class Project2 implements ActionListener {
         b5.addActionListener(this);
 
 
+        t2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Deposit Amount $" + t2.getText());
+            }
+        });
+
+
+
+
+        t1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Withdraw Amount $" + t1.getText());
+            }
+        });
+
+
+
     }
 
     public void actionPerformed(ActionEvent e)
@@ -179,19 +221,18 @@ class Project2 implements ActionListener {
 
     public static void main(String ag[])
     {
-        quz p4=new quz();
+        Bank p4=new Bank();
     }
 }
 
 //Second JFrame
 
 
-class Project3
-{
+class Project3 implements ActionListener {
     JFrame f1=new JFrame("New Account");
     JLabel l1,l2;
     JTextField t1,t2;
-    JButton b1;
+    JButton b9;
 
     Project3()
     {
@@ -199,7 +240,7 @@ class Project3
         l2=new JLabel("Initial Deposit:");
         t1=new JTextField(20);
         t2=new JTextField(20);
-        b1=new JButton("Create Account");
+        b9=new JButton("Create Account");
 
         f1.setSize(400,400);
         f1.setVisible(true);
@@ -211,7 +252,7 @@ class Project3
         f1.add(l2);
         f1.add(t2);
 
-        f1.getContentPane().add(b1);
+        f1.getContentPane().add(b9);
         Container c = f1.getContentPane();
         c.setBackground(Color.LIGHT_GRAY);
 
@@ -219,15 +260,91 @@ class Project3
         t1.setBounds(110,30,100,30);
         l2.setBounds(0,70,100,30);
         t2.setBounds(110,70,100,30);
-        b1.setBounds(50,120,200,30);
+        b9.setBounds(50,120,200,30);
+
+        b9.addActionListener(this);
+
+
+        t2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Deposit Amount $" + t2.getText());
+            }
+        });
 
 
 
 
-        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f1.setVisible(true);
-        f1.setSize(400,400);
+        t1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Withdraw Amount $" + t1.getText());
+            }
+        });
+
+
+
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        f1.dispose();
+        Project5 p4=new Project5();
+    }
+
+    public static void main(String ag[])
+    {
+        Bank p5=new Bank();
     }
 }
 
+class Project5 implements ActionListener {
+    JFrame f1 = new JFrame("Banking");
+
+
+    JButton b0;
+    JLabel l3;
+
+
+
+    Project5() {
+        JLabel l3 = new JLabel("Account Created", SwingConstants.CENTER);
+        l3.setFont(new Font("Serif", Font.BOLD, 20));
+
+
+
+        b0 = new JButton("Checking");
+
+
+        f1.setSize(400, 400);
+        f1.setVisible(true);
+        f1.setLayout(null);
+
+        f1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+
+        f1.add(l3);
+        
+
+
+        f1.getContentPane().add(b0);
+        Container c = f1.getContentPane();
+        c.setBackground(Color.LIGHT_GRAY);
+
+
+        b0.setBounds(145, 100, 100, 30);
+
+
+
+        l3.setBounds(100, 10, 200, 30);
+
+
+        f1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
 
